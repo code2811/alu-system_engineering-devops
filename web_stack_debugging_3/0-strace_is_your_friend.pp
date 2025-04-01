40048c8c82e9cd1742ca1753d25a1667c7c8dfd0
code2811 <f.kayiranga@alustudent.com>
2025-04-01 21:06:45 +0000

    0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,6 @@
-# Puppet manifest to fix the Apache 500 error
-
-# The most common WordPress 500 errors are often due to:
-# 1. PHP misconfigurations (file permissions, paths, etc.)
-# 2. Typos in PHP file names causing "file not found" errors
-# 3. Incorrect file paths in WordPress configuration
-
-# Based on strace output, the issue is likely a misspelled PHP file extension
-# in the WordPress configuration (e.g., '.phpp' instead of '.php')
-
+# Fixes Apache 500 error by correcting typo in WordPress configuration file
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin:/usr/bin:/bin',
   onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
-}}
+}}}
