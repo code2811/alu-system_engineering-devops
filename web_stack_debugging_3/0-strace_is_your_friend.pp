a4eef8bd8146fd33106727cb50c176b933cbd451
code2811 <f.kayiranga@alustudent.com>
2025-04-01 21:16:23 +0000

    0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes Apache 500 error by correcting typo in WordPress configuration file
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin:/usr/bin:/bin',
-  onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
-}}}
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -f /var/www/html/wp-settings.php && grep -q phpp /var/www/html/wp-settings.php',
+}
