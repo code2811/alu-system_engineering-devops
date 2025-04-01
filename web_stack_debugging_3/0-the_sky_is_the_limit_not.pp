e467da2c69d8d54fe808e8aaae062e50862f7c2f
code2811 <f.kayiranga@alustudent.com>
2025-04-01 21:27:20 +0000

    0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_3/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_3/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
-# Fixes Nginx failed requests issue by increasing ULIMIT
+# Increases Nginx ULIMIT to handle high traffic and eliminate failed requests
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx && service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command  => 'sed -i "s/15/4096/" /etc/default/nginx && service nginx restart',
+  path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   provider => shell,
 }
