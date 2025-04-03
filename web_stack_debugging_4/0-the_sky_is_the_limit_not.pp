0bd5787e15758cf2e667bea65a36d0073e5cf7a3
code2811 <f.kayiranga@alustudent.com>
2025-04-03 17:59:29 +0000

    0-the_sky_is_the_limit_not.pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,21 @@
-# Increases the ULIMIT of the default file
+# Puppet manifest to increase Nginx performance under high load
+# Fix for benchmarking issue where 943 out of 2000 requests were failing
+
+# Increase the ULIMIT for the Nginx service
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && service nginx restart',
+  path    => '/usr/local/bin/:/bin/:/usr/bin/:/usr/sbin/',
 }
 
-# Restart Nginx
--> service { 'nginx':
-  ensure => running,
-  enable => true,
-}}
+# Alternative approach could include updating the Nginx configuration directly:
+# file { '/etc/nginx/nginx.conf':
+#   ensure  => present,
+#   content => template('nginx/nginx.conf.erb'),
+#   notify  => Service['nginx'],
+# }
+# 
+# service { 'nginx':
+#   ensure  => running,
+#   enable  => true,
+#   require => File['/etc/nginx/nginx.conf'],
+# }
